f586e5eb480298bb204a5016bb31595c4b920b88
poseidon2022 <kidusm82@gmail.com>
2025-06-21 01:06:41 +0300

    feat: auth and validation logic initialized

--- src/miidleware/validateRequest.middleware.ts
+++ src/miidleware/validateRequest.middleware.ts
@@ -10,11 +10,11 @@ export const handleServiceResponse = (serviceResponse: ServiceResponse<any>, res
 
 export const validateRequest = (schema: ZodSchema) => (req: Request, res: Response, next: NextFunction) => {
   try {
-    // Parsing the request using the schema
+
     schema.parse({ body: req.body, query: req.query, params: req.params });
     next();
   } catch (err) {
-    // Check if the error is a ZodError
+
     if (err instanceof ZodError) {
       // Mapping each error to extract the field path and message
       const errorDetails = err.errors.map((e) => ({
@@ -26,8 +26,6 @@ export const validateRequest = (schema: ZodSchema) => (req: Request, res: Respon
       const serviceResponse = ServiceResponse.failure("Invalid input", errorDetails, statusCode);
       res.status(serviceResponse.statusCode).send(serviceResponse);
     }
-
-    // Handle other types of errors (if necessary)
     next(err);
   }
 };
